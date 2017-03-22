a52f4fb7328de7e15840f8b7c050f5e36028030c
Timur Isanov <xtimon@yahoo.com>
2017-03-22 13:56:27 +0300

    mirrors count

--- add_mirrors.sh
+++ add_mirrors.sh
@@ -2,11 +2,12 @@
 
 MIRRORS_LIST="http://rsync.alpinelinux.org/alpine/MIRRORS.txt"
 ALPINE_VERSION="v$(cut -d. -f1 /etc/alpine-release).$(cut -d. -f2 /etc/alpine-release)"
+MIRRORS_COUNT=5
 
 TEMP_FILE=$(mktemp)
 wget $MIRRORS_LIST -O $TEMP_FILE
 
-for mirror in $(cat $TEMP_FILE)
+for mirror in $(head -$MIRRORS_COUNT $TEMP_FILE)
 do
   echo "${mirror}$ALPINE_VERSION/main" >> /etc/apk/repositories
   echo "${mirror}$ALPINE_VERSION/community" >> /etc/apk/repositories
