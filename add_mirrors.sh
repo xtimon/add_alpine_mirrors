e33c099888f39ad8b1185b76cc8fca632a39cf58
Timur Isanov <xtimon@yahoo.com>
2017-03-22 14:10:14 +0300

    commit

--- add_mirrors.sh
+++ add_mirrors.sh
@@ -8,8 +8,8 @@ wget ${MIRRORS_LIST:="http://rsync.alpinelinux.org/alpine/MIRRORS.txt"} -O $TEMP
 
 for mirror in $(head -${MIRRORS_COUNT:=5} $TEMP_FILE)
 do
-  echo "${mirror}$ALPINE_VERSION/main" >> /etc/apk/repositories
-  echo "${mirror}$ALPINE_VERSION/community" >> /etc/apk/repositories
+  echo "${mirror}${ALPINE_VERSION}/main" >> /etc/apk/repositories
+  echo "${mirror}${ALPINE_VERSION}/community" >> /etc/apk/repositories
 done
 
 rm $TEMP_FILE $0
