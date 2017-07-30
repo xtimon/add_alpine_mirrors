3ac63a548fc5b1fc2c9dff39c272d91010b1b52e
Timur Isanov <xtimon@yahoo.com>
2017-07-30 13:38:01 +0300

    trap function

--- add_mirrors.sh
+++ add_mirrors.sh
@@ -4,6 +4,12 @@ ALPINE_VERSION="v$(cut -d. -f1 /etc/alpine-release).$(cut -d. -f2 /etc/alpine-re
 
 TEMP_FILE=$(mktemp)
 
+function clear {
+  rm $TEMP_FILE $0
+}
+
+trap clear EXIT
+
 wget ${MIRRORS_LIST:="http://rsync.alpinelinux.org/alpine/MIRRORS.txt"} -O $TEMP_FILE
 
 for mirror in $(head -${MIRRORS_COUNT:=5} $TEMP_FILE)
@@ -11,5 +17,3 @@ do
   echo "${mirror}${ALPINE_VERSION}/main" >> /etc/apk/repositories
   echo "${mirror}${ALPINE_VERSION}/community" >> /etc/apk/repositories
 done
-
-rm $TEMP_FILE $0
